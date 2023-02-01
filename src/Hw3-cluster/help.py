6ddf3d487304ea43b1972a9324f9a578aafbd079
Mansimran Singh Anand <mansimransinghanand@gmail.com>
2023-02-01 17:08:16 -0500

    updating help class

--- src/Hw3-cluster/help.py
+++ src/Hw3-cluster/help.py
@@ -10,19 +10,22 @@ the = {
     'seed': 937162211,
 }
 
-help=" \n\
-data.lua : an example csv reader script\n\
-(c)2022, Tim Menzies <timm@ieee.org>, BSD-2\n\
-USAGE:   data.lua  [OPTIONS] [-g ACTION]\n\
-OPTIONS:\n\
-  -d  --dump  on crash, dump stack = false\n\
-  -f  --file  name of file         = ../../etc/data/auto93.csv\n\
-  -g  --go    start-up action      = data\n\
-  -h  --help  show help            = false\n\
-  -s  --seed  random number seed   = 937162211\n\
-ACTIONS:\n\
+help="\n\
+cluster.lua : an example csv reader script \n\
+(c)2022, Tim Menzies <timm@ieee.org>, BSD-2 \n\
+USAGE: cluster.lua  [OPTIONS] [-g ACTION] \n\
+OPTIONS: \n\
+  -d  --dump    on crash, dump stack   = false \n\
+  -f  --file    name of file           = ../etc/data/auto93.csv \n\
+  -F  --Far     distance to 'faraway'  = .95 \n\
+  -g  --go      start-up action        = data \n\
+  -h  --help    show help              = false \n\
+  -m  --min     stop clusters at N^min = .5 \n\
+  -p  --p       distance coefficient   = 2 \n\
+  -s  --seed    random number seed     = 937162211 \n\
+  -S  --Sample  sampling data size     = 512 \n\
+ACTIONS: \n\
 "
-
 Seed = 937162211
 
 egs={}
