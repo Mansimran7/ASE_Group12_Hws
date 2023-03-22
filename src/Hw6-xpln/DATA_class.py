53496aff7acb31d443b062aa697fef3dd6cfcb9a
devanshi39 <devanshik.savla123@gmail.com>
2023-03-22 14:21:47 -0400

    hw6 10 test cases passing

--- src/Hw6-xpln/DATA_class.py
+++ src/Hw6-xpln/DATA_class.py
@@ -78,39 +78,34 @@ class DATA:
     def furthest(self, row1, rows = None, cols = None):
         t = self.around(row1,rows,cols)
         return t[len(t)-1]
-    
+
     def half(self, rows = None, cols = None, above = None):
         def project(row):
-            x, y = misc.cosine(dist(row,A), dist(row,B), c)
-            try:
-                row.x = row.x
-                row.y = row.y
-            except:
-                row.x = x
-                row.y = y
-            return {'row' : row, 'x' : x, 'y' : y}
-            # return {'row' : row, 'dist' : cosine(dist(row,A), dist(row,B), c)}
-        
-        def dist(row1,row2): 
-            return self.dist(row1,row2,cols)
-        
+            return {'row' : row, 'dist' : cosine(gap(row,A), gap(row,B), c)}
+
+        def gap(r1,r2): 
+            return self.dist(r1,r2,cols)
+
+        def function(r):
+            return {'row' : r, 'dist' : gap(r, A)}
+
         rows = rows or self.rows
-        # some = many(rows,the['Sample'])
-        A = above or rows[1]
-        B = self.furthest(A,rows)['row']
-        c = dist(A,B)
+        some = many(rows,the['Halves'])
+        A    = above if above and the['Reuse'] else any(some)
+        temp = sorted(list(map(function, some)), key = lambda k : k["dist"])
+        far = temp[int(the['Far'] * len(rows))//1]
+        B    = far['row']
+        c    = far['dist']
         left, right = [], []
-        
-        pairs_of_prows = sorted(list(map(project, rows)), key=lambda k: k["x"])
-        
-        for n, temp in enumerate(pairs_of_prows):
-            if   n < len(rows) // 2:
-                left.append(temp["row"])
-                mid = temp["row"]
+
+        for n,temp in enumerate(sorted(list(map(project, rows)), key = lambda k : k["dist"])):
+            if n < len(rows)//2:
+                left.append(temp['row'])
             else:
-                right.append(temp["row"])
-        
-        return left, right, A, B, mid, c
+                right.append(temp['row'])
+
+        evals = 1 if the['Reuse'] and above else 2
+        return left, right, A, B, c, evals
 
     def cluster(self, rows = None, cols = None, above = None):
         rows = rows or self.rows
@@ -122,16 +117,29 @@ class DATA:
             node['left'] =  self.cluster(left, cols, node['A'])
             node['right'] = self.cluster(right, cols, node['B'])
         return node
-        
 
-    def sway(self, rows = None, min = None, cols = None, above = None):
+    def tree(self, rows = None , mini = None, cols = None, above = None):
         rows = rows or self.rows
-        min = min or math.pow(len(rows), the['min'])
+        mini  = mini or len(rows)**the['min']
         cols = cols or self.cols.x
-        node = {'data' : self.clone(rows)}
-        if len(rows) > 2*min:
+        node = { 'data' : self.clone(rows) }
+        if len(rows) >= 2*mini:
             left, right, node['A'], node['B'], node['mid'], _ = self.half(rows,cols,above)
-            if self.better(node['B'],node['A']):
-                left,right,node['A'],node['B'] = right,left,node['B'],node['A']
-            node['left']  = self.sway(left,  min, cols, node['A'])
+            node['left']  = self.tree(left,  mini, cols, node['A'])
+            node['right'] = self.tree(right, mini, cols, node['B'])
         return node
+    
+    def sway(self):
+        data = self
+        def worker(rows, worse, evals = None, above = None):
+            if len(rows) <= len(data.rows)**the['min']: 
+                return rows, misc.many(worse, the['rest']*len(rows)), evals
+            else:
+                l,r,a,b,c,evals1 = self.half(rows, None, above)
+                if self.better(b,a):
+                    l,r,a,b = r,l,b,a
+                for row in r:
+                    worse.append(row)
+                return worker(l,worse,evals+evals1,a)
+        best,rest,evals1 = worker(data.rows,[],0)
+        return self.clone(best), self.clone(rest), evals1
