d37e1ececb8b323657a2ac90aadf73e1bf366864
banerjeeindranil350@gmail.com <banerjeeindranil350@gmail.com>
2023-03-29 18:56:29 -0400

    Added basic test case function

--- tests/testEngine.py
+++ tests/testEngine.py
@@ -281,3 +281,19 @@ def bootmu_test():
         bs = misc.bootstrap(a, b)
         print("",mu,1,cl,bs,cl and bs)
         mu = mu + 0.1
+
+def basic_test():
+    print("\t\ttruee", bootstrap( {8, 7, 6, 2, 5, 8, 7, 3}, 
+                                {8, 7, 6, 2, 5, 8, 7, 3}),
+              cliffsDelta( {8, 7, 6, 2, 5, 8, 7, 3}, 
+                           {8, 7, 6, 2, 5, 8, 7, 3}))
+    print("\t\tfalse", bootstrap(  {8, 7, 6, 2, 5, 8, 7, 3},  
+                                 {9, 9, 7, 8, 10, 9, 6}),
+             cliffsDelta( {8, 7, 6, 2, 5, 8, 7, 3},  
+                          {9, 9, 7, 8, 10, 9, 6})) 
+    print("\t\tfalse", 
+                    bootstrap({0.34, 0.49, 0.51, 0.6,   .34,  .49,  .51, .6}, 
+                               {0.6,  0.7,  0.8,  0.9,   .6,   .7,   .8,  .9}),
+                  cliffsDelta({0.34, 0.49, 0.51, 0.6,   .34,  .49,  .51, .6}, 
+                              {0.6,  0.7,  0.8,  0.9,   .6,   .7,   .8,  .9}))
+
